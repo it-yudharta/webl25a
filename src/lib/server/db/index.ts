3109d2d5aec16d1a0b1d8a4abf20cc0d4ba33a1a
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2026-04-23 15:22:05 +0700

    getDB on server

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,4 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/d1';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = createClient({ url: env.DATABASE_URL });
-
-export const db = drizzle(client, { schema });
+export const getDb = (d1: D1Database) => drizzle(d1, { schema });
